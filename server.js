211b9db3096bffcffd01d1b4c4cfa81af022b672
Emmanuel-Narrys <73914870+Emmanuel-Narrys@users.noreply.github.com>
2025-12-10 01:00:24 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,17 @@
 const express = require('express');
 const next = require('next');
 
-const port = process.env.PORT || 3000
-const dev = process.env.NODE_ENV !== 'production';
+const port = 3000;
+const dev = false;
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
     const server = express();
 
-    // All other routes handled by Next.js
-    server.all('*', (req, res) => {
-        return handle(req, res);
-    });
+    server.all('*', (req, res) => handle(req, res));
 
-    server.listen(port, err => {
-        if (err) throw err;
-        console.log(`> Ready on http://localhost:${port}`);
+    server.listen(port, () => {
+        console.log("Ready on port 3000");
     });
 });
